5ea900b0c21bfd8d3307e44646407ce9a588ff0b
minhai4wrk <minhhai4058@gmail.com>
2026-03-17 01:23:17 +0700

    update main with db

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from src import create_app
 from flask import Flask
-from models import db
+from src.models import db
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+# connect MySQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'mysql+pymysql://root:nongminhhai123@localhost:3306/quan_ly_giang_vien'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'bi_mat'
+
+# Khởi tạo SQLAlchemy
+db.init_app(app)
+
+# routes
+import src.admin
+import src.auth_user
+import src.user
+
+if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
